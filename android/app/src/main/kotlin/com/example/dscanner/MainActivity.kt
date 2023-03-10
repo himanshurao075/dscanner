75ac3c3fffdc9b9ce039c1c987e98ef301f385eb
ranveer <ranveergour781@gmail.com>
2023-03-10 17:44:14 +0530

    opencv load lib

--- android/app/src/main/kotlin/com/example/dscanner/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dscanner/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.dscanner
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.util.Log
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import org.opencv.android.Utils
 import org.opencv.core.Mat
+import org.opencv.android.LoaderCallbackInterface
+import org.opencv.android.BaseLoaderCallback
+import org.opencv.android.OpenCVLoader
 
 import org.opencv.core.MatOfPoint2f
 import org.opencv.core.Point
@@ -17,68 +21,92 @@ import org.opencv.imgproc.Imgproc
 import java.io.File
 
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity(){
     private val CHANNEL = "samples.flutter.dev/cropImage"
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            if (call.method == "cropImage") {
+                println("Flow 2")
 
-override  fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-    super.configureFlutterEngine(flutterEngine)
-    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-         call, result ->
-      if (call.method == "cropImage") {
-        println("Flow 2")
+                val x1: Double = call.argument("x1")!!
+                val x2: Double = call.argument("x2")!!
+                val x3: Double = call.argument("x3")!!
+                val x4: Double = call.argument("x4")!!
+                val y1: Double = call.argument("y1")!!
+                val y2: Double = call.argument("y2")!!
+                val y3: Double = call.argument("y3")!!
+                val y4: Double = call.argument("y4")!!
+                val imgWidth: Int = call.argument("width")!!
+                val imgHeight: Int = call.argument("height")!!
+                val imgPath: String = call.argument("imgPath")!!
+                println("Flow 1")
 
-        val x1 : Double = call.argument("x1")!!
-        val x2 : Double = call.argument("x2")!!
-        val x3 : Double = call.argument("x3")!!
-        val x4 : Double = call.argument("x4")!!
-        val y1 : Double = call.argument("y1")!!
-        val y2 : Double = call.argument("y2")!!
-        val y3 : Double = call.argument("y3")!!
-        val y4 : Double = call.argument("y4")!!
-        val imgWidth : Int = call.argument("width")!!
-        val imgHeight : Int = call.argument("height")!!
-        val imgPath : String = call.argument("imgPath")!!
-        println("Flow 1")
-
-        val croppedImage = cropImage(x1,x2,x3,x4,y1,y2,y3,y4,imgWidth.toDouble(), imgHeight.toDouble(),imgPath)
-        result.success(croppedImage)
+                val croppedImage = cropImage(
+                    x1,
+                    x2,
+                    x3,
+                    x4,
+                    y1,
+                    y2,
+                    y3,
+                    y4,
+                    imgWidth.toDouble(),
+                    imgHeight.toDouble(),
+                    imgPath
+                )
+                result.success(croppedImage)
 
 //
 //        if (batteryLevel != -1) {
 //          } else {
 //          result.error("UNAVAILABLE", "Battery level not available.", null)
 //        }
-      } else {
-        result.notImplemented()
-      }
+            } else {
+                result.notImplemented()
+            }
+        }
     }
-  }
 
 
-
-  private fun cropImage(x1: Double, x2: Double, x3: Double, x4: Double, y1: Double, y2: Double, y3: Double, y4: Double,imgWidth: Double,imgHeight : Double,inputImg : String): String {
+    private fun cropImage(
+        x1: Double,
+        x2: Double,
+        x3: Double,
+        x4: Double,
+        y1: Double,
+        y2: Double,
+        y3: Double,
+        y4: Double,
+        imgWidth: Double,
+        imgHeight: Double,
+        inputImg: String
+    ): String {
 //    val sd: File = Environment.getExternalStorageDirectory()
-    println("Flow 3")
-    val image = File(inputImg)
-    print("Image Path ${image.path}");
-  try {
-    println("Flow 4")
-    val bmOptions: BitmapFactory.Options = BitmapFactory.Options()
-    var bitmap: Bitmap = BitmapFactory.decodeFile(image.path, bmOptions)
-    bitmap = Bitmap.createScaledBitmap(bitmap, imgWidth.toInt(), imgHeight.toInt(), true)
-    println("Flow 5")
-    val imagePath: String
-    val sortedPoints = arrayOfNulls<Point>(4)
-    val point1 = Point(x1, y1 )
-    val point2 = Point(x2,y2)
-    val point3 = Point(x3,y3)
-    val point4 = Point(x4,y4 )
-    println("Flow 6")
+        println("Flow 3")
+        val image = File(inputImg)
+        print("Image Path ${image.path}");
+        try {
+            println("Flow 4")
+            val bmOptions: BitmapFactory.Options = BitmapFactory.Options()
+            var bitmap: Bitmap = BitmapFactory.decodeFile(image.path, bmOptions)
+            bitmap = Bitmap.createScaledBitmap(bitmap, imgWidth.toInt(), imgHeight.toInt(), true)
+            println("Flow 5")
+            val imagePath: String
+            val sortedPoints = arrayOfNulls<Point>(4)
+            val point1 = Point(x1, y1)
+            val point2 = Point(x2, y2)
+            val point3 = Point(x3, y3)
+            val point4 = Point(x4, y4)
+            println("Flow 6")
 
 //    MatOfPoint2f src = MatOfPoint2f()
-    val src = MatOfPoint2f(
-            point1,point2,point3,point4
-    )
+            val src = MatOfPoint2f(
+                point1, point2, point3, point4
+            )
 
 
 //    val src = MatOfPoint2f(
@@ -86,38 +114,50 @@ override  fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
 //            sortedPoints.get(1),
 //            sortedPoints.get(2),
 //            sortedPoints.get(3))
-    println("Flow 7")
-    val currentImage = Mat()
+            println("Flow 7")
+            val currentImage = Mat()
 //    val bmp32: Bitmap = bmp.copy(Bitmap.Config.ARGB_8888, true)
-    Utils.bitmapToMat(bitmap, currentImage)
+            Utils.bitmapToMat(bitmap, currentImage)
 
 //                src.adjustROI(lineWidth, lineWidth, lineWidth, lineWidth);
-    println("Flow 8")
+            println("Flow 8")
 
-    val dst = MatOfPoint2f(
-            Point(0.0, 0.0),
-            Point(imgWidth.toDouble(), 0.0),
-            Point(0.0, imgHeight),
-            Point(imgWidth, imgHeight)
-    )
-    println("Flow 9")
-    val warpMat = Imgproc.getPerspectiveTransform(src, dst)
-    //This is your new image as Mat
-    val destImage = Mat()
-    println("Flow 9")
-    // Imgproc.cvtColor(currentImage,destImage,Imgproc.COLOR_RGBA2GRAY);
-    Imgproc.warpPerspective(currentImage, destImage, warpMat, Size(imgWidth,imgHeight))
+            val dst = MatOfPoint2f(
+                Point(0.0, 0.0),
+                Point(imgWidth.toDouble(), 0.0),
+                Point(0.0, imgHeight),
+                Point(imgWidth, imgHeight)
+            )
+            println("Flow 9")
+            val warpMat = Imgproc.getPerspectiveTransform(src, dst)
+            //This is your new image as Mat
+            val destImage = Mat()
+            println("Flow 9")
+            // Imgproc.cvtColor(currentImage,destImage,Imgproc.COLOR_RGBA2GRAY);
+            Imgproc.warpPerspective(currentImage, destImage, warpMat, Size(imgWidth, imgHeight))
 
-    println("Flow 10")
-  }
-  catch (e: Exception) {
-    println(e)
-    return "EXP : $e"
-  }
+            println("Flow 10")
+        } catch (e: Exception) {
+            println(e)
+            return "EXP : $e"
+        }
 
 
 //  return imagePath
-    return "Pass String"
-  }
+        return "Pass String"
+    }
+
+    private val mLoaderCallback: BaseLoaderCallback = object : BaseLoaderCallback(this) {
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback)
+        } else {
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS)
+        }
+    }
 }
 
